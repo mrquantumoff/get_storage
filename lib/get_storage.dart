2acb4bf2091e38893e1197f6a59dd95bf5f1c9ea
Demir Yerli <mrquantumoff@protonmail.com>
2024-03-09 09:59:49 +0300

    change the name of the crate and remove some of the useless code

--- lib/get_storage.dart
+++ lib/get_storage.dart
@@ -1,5 +1,5 @@
-library get_storage;
+library get_storage_qnt;
 
-export 'package:get_storage/src/read_write_value.dart';
-export 'package:get_storage/src/storage_impl.dart';
-export 'package:get_storage/src/value.dart';
+export 'package:get_storage_qnt/src/read_write_value.dart';
+export 'package:get_storage_qnt/src/storage_impl.dart';
+export 'package:get_storage_qnt/src/value.dart';
--- lib/src/read_write_value.dart
+++ lib/src/read_write_value.dart
@@ -1,4 +1,4 @@
-import 'package:get_storage/src/storage_impl.dart';
+import 'package:get_storage_qnt/src/storage_impl.dart';
 
 typedef StorageFactory = GetStorage Function();
 
